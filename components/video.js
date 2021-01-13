e8b5e2c29e946d458e8b2ce906f19e99f82ca8df
Alex Siqueira Lacerda <lequizi@gmail.com>
2021-01-13 15:12:14 -0300

    adding oq havera

--- components/video.js
+++ components/video.js
@@ -6,7 +6,7 @@ const Video = () => {
     return (
         <section className={styles.section}>
             <h1 className={styles.titulo}> IMERSÃO ONLINE DE <span className={styles.bgblue}>7 DIAS</span> APRENDENDO <span className={styles.bgblue}>TUDO QUE SEI</span> SOBRE <span className={styles.bgblue}>VIAGENS</span></h1>
-            <iframe className={styles.video} src="https://www.youtube.com/embed/l7-tUzJNMMQ" frameborder="0" allow="autoplay; encrypted-media"></iframe>
+            <iframe className={styles.video} src="https://www.youtube.com/embed/l7-tUzJNMMQ" frameborder="0" allow="autoplay; fullscreen; encrypted-media"></iframe>
             <div className={styles.btn}>
             <BtnQueroEntrar/>
 
