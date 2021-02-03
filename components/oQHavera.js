47e6ea61b5d16086bb41ec9a85558e1b8e87b45d
Alex Siqueira Lacerda <lequizi@gmail.com>
2021-02-03 12:15:51 -0300

    a

--- components/oQHavera.js
+++ components/oQHavera.js
@@ -10,6 +10,9 @@ const OQHavera = () => {
                     <p>Eu percebi durante a pandemia que muitas pessoas abriram os olhos para o quão <strong>curta é a vida</strong> e tem que ser aproveitada, olhos que já abri 6 anos atrás e comecei a <strong>viajar o mundo</strong> para experimentar de tudo e viver a cada segundo. Desde então gravo vídeos para o Youtube mostrando como <strong>a vida é legal.</strong> Mas dessa vez eu resolvi criar uma imersão online onde vou dar uma pausa na minha viagem por <strong>1 semana e ensinar exatamente TUDO o que sei sobre viagens!</strong></p>
                     <p>Serão 7 dias onde eu vou estar <strong>100% disponível para conversar com um número limitado de participantes</strong>. Teremos conteúdos AO VIVASSO, aulas épicas por vídeo, ebook e grupos com participantes! Fique atento porque vou ligar de surpresa para alguns participantes no meio dessa semana! Cada dia da semana teremos um conteúdo diferente sobre um modo diferente de viagem, <strong>modos que sou especialista!</strong></p>
                 </div>
+                <div className={styles.setedias}>
+                    <img height="350" src="/7-dias.png"></img>
+                </div>
             </div>
         </section>
     )
