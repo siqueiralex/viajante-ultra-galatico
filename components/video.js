bcee7f53edfee78c76a6921791c8dc38c82264ae
Alex Siqueira Lacerda <lequizi@gmail.com>
2021-02-08 18:11:32 -0300

    a

--- components/video.js
+++ components/video.js
@@ -6,13 +6,13 @@ import BtnQueroEntrar from '../components/btnqueroentrar.js'
 const Video = () => {
     return (
         <section className={styles.section}>
-            <h1 className={styles.titulo}> IMERSÃO ONLINE DE <span className={styles.bgblue}>7 DIAS</span> APRENDENDO <span className={styles.bgblue}>TUDO QUE SEI</span> SOBRE <span className={styles.bgblue}>VIAGENS</span></h1>
-            <iframe className={styles.video} src="https://www.youtube.com/embed/l7-tUzJNMMQ" frameborder="0" allow="autoplay; fullscreen; encrypted-media"></iframe>
-            <div className={styles.btn}>
-                
+            {/* <h1 className={styles.titulo}> IMERSÃO ONLINE DE <span className={styles.bgblue}>7 DIAS</span> APRENDENDO <span className={styles.bgblue}>TUDO QUE SEI</span> SOBRE <span className={styles.bgblue}>VIAGENS</span></h1> */}
             <div className={btnStyles.btnWrapper}>
                 <BtnQueroEntrar/>
             </div>
+            <iframe className={styles.video} src="https://www.youtube.com/embed/l7-tUzJNMMQ" frameborder="0" allow="autoplay; fullscreen; encrypted-media"></iframe>
+            <div className={styles.btn}>
+                
 
             </div>
 
