3c72ff392fb7a4594acbac97501dbeea7a62ebd6
Alex Siqueira Lacerda <lequizi@gmail.com>
2021-02-23 01:44:55 -0300

    a

--- components/video.js
+++ components/video.js
@@ -6,7 +6,7 @@ const Video = () => {
         <section className={styles.section}>
             {/* <h1 className={styles.titulo}> IMERSÃO ONLINE DE <span className={styles.bgblue}>7 DIAS</span> APRENDENDO <span className={styles.bgblue}>TUDO QUE SEI</span> SOBRE <span className={styles.bgblue}>VIAGENS</span></h1> */}
             
-            <iframe loading="lazy" className={styles.video} src="https://www.youtube.com/embed/R-T-lm_2Gfs" frameborder="0" allow="autoplay; fullscreen; encrypted-media"></iframe>
+            <iframe loading="lazy" className={styles.video} src="//www.youtube.com/embed/R-T-lm_2Gfs?enablejsapi=1&amp;controls=1&amp;autoplay=1&amp;disablekb=1&amp;fs=1&amp;rel=0&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=1&amp;loop=1&amp;origin=http://viajanteultragalatico.com.br" frameborder="0" allow="autoplay; fullscreen; encrypted-media" allowTransparency="true"></iframe>
             <div className={styles.btn}>
                 
 
