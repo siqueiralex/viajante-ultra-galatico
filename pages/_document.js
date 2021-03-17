b7e62f53c669d7f2ce972dde3ca4136d4c646ed7
Alex Siqueira Lacerda <siqueiralex@Alexs-MacBook-Pro.local>
2021-03-16 22:42:44 -0300

    a

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,21 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-br">
-        <Head />
+        <Head>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `<script async src="https://www.googletagmanager.com/gtag/js?id=G-YBWYYQH83X"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)}
+                gtag('js', new Date());
+    
+                gtag('config', 'G-YBWYYQH83X');
+              </script>`
+            }}
+          />
+        
+        </Head>
         <body>
           <Main />
           <NextScript />
